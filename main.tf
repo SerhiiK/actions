727dfce320b3cd2b09c0ece7d5bade660e940715
Serhii Krupskyi <6170876+SerhiiK@users.noreply.github.com>
2022-05-19 19:14:07 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-resource "google_compute_network" "vpc_network_default" {
+resource "google_compute_network" "vpc_network_default_maybe" {
   name = "terraform-network3"
   description = "bla bla bla"
 }
