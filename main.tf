bcb1e4d1e8027948d6d65a30c6bff44f0d235b07
Serhii Krupskyi <6170876+SerhiiK@users.noreply.github.com>
2022-05-19 19:28:59 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,3 +18,14 @@ resource "google_compute_network" "vpc_network_default_maybe" {
   description = "bla bla bla more bla"
 }
 
+resource "google_compute_backend_bucket" "image_backend" {
+  name        = "image-backend-bucket"
+  description = "Contains beautiful images"
+  bucket_name = google_storage_bucket.image_bucket.name
+  enable_cdn  = true
+}
+
+resource "google_storage_bucket" "image_bucket" {
+  name     = "image-store-bucket"
+  location = "EU"
+}
